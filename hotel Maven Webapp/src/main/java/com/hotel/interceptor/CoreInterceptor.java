d9e61dc348b89c16bcf4b26188a7f35ea1c5f92f
HoYuanhao <727869774@qq.com>
2018-05-15 21:09:04 +0800

    完善拦截器备注添加了日志功能

--- hotel Maven Webapp/src/main/java/com/hotel/interceptor/CoreInterceptor.java
+++ hotel Maven Webapp/src/main/java/com/hotel/interceptor/CoreInterceptor.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.hotel.util.PropertiesUtil;
 
 public class CoreInterceptor implements HandlerInterceptor{
+private final Logger log=LoggerFactory.getLogger(CoreInterceptor.class);
 private static Map<String,String> anonymous;
 private static Map<String,String> needLogin;
 static {
@@ -35,7 +38,7 @@ public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object
 /**
  * @author yuanhaohe
  * 配置完匿名权限的地址将允许所有用户访问，也可以在needlogin中配置地址（配置后用户需要登录后才能访问，否则返回code 400）配置信息详见properties
- * 注:配置完匿名用户地址权限后，将会覆盖有名用户权限（所有URL必须在其中一个配置文件中配置，否则会被拦截）
+ * 注:配置完匿名用户地址权限后，将会覆盖有名用户权限（所有URL必须在其中一个配置文件中配置，否则会被拦截）展示在session里存了user表示为登录，到时候再商定
  * @return 是否执行controller
  */
 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
@@ -43,6 +46,7 @@ public boolean preHandle(HttpServletRequest request, HttpServletResponse respons
 	//允许匿名用户获取资源
 	for(String key:anonymousKey){
 		if(request.getRequestURI().contains(key)){
+			log.debug("匿名访问:"+request.getRequestURI());
 			return true;
 		}
 	}
@@ -53,6 +57,7 @@ public boolean preHandle(HttpServletRequest request, HttpServletResponse respons
 			HttpSession session=request.getSession();
 			String user=(String) session.getAttribute("user");
 			if(user!=null){
+				log.debug("实名访问:"+request.getRequestURI());
 				return true;
 			}
 		}
@@ -60,6 +65,7 @@ public boolean preHandle(HttpServletRequest request, HttpServletResponse respons
     response.setStatus(HttpStatus.OK.value()); //设置状态码  
 	response.setContentType("application/json;charset=utf-8");
 	response.getWriter().write("{\"code\":400,\"msg\":\"No Aythority\"}");
+	log.debug("实名被拒绝:"+request.getRequestURI());
 	return false;
 }
 }
