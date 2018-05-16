29ee300aaf31edf67146319c69a4ab85ae9e5d9e
HoYuanhao <727869774@qq.com>
2018-05-16 16:19:33 +0800

    增加redis

--- hotel Maven Webapp/src/main/java/com/hotel/controller/TestController.java
+++ hotel Maven Webapp/src/main/java/com/hotel/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.hotel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hotel.api.service.BaiduMapService;
 import com.hotel.jms.producer.TestProducer;
+import com.hotel.redis.RedisService;
 import com.hotel.service.TestService;
 
 @Controller
@@ -18,6 +20,8 @@ public class TestController {
 	private BaiduMapService baiduMapService;
 	@Autowired
 	private TestProducer producer;
+	@Autowired
+	private RedisService redisService;
 @RequestMapping(value="test.json",method=RequestMethod.GET,produces="application/json;charset=utf-8")
 
 	public @ResponseBody String test(){
@@ -27,6 +31,9 @@ public class TestController {
 		producer.sendMessage(city);
 		System.out.println(city);
 		System.out.println(c);
+		System.out.println("start redis-----");
+		redisService.set("username", "123456");
+		System.out.println(redisService.get("username"));
 	} catch (Exception e) {
 		// TODO 自动生成的 catch 块
 		e.printStackTrace();
