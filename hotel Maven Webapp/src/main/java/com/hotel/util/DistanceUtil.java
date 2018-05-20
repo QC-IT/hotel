2406055993d4fff96cba78193ac30b8749d33354
HoYuanhao <727869774@qq.com>
2018-05-20 19:55:11 +0800

    更新

--- hotel Maven Webapp/src/main/java/com/hotel/util/DistanceUtil.java
+++ hotel Maven Webapp/src/main/java/com/hotel/util/DistanceUtil.java
@@ -5,7 +5,9 @@ package com.hotel.util;
  *
  */
 public final class DistanceUtil {
+	
 	 private static double EARTH_RADIUS = 6378.137; 
+	 
 	 /**
      * 根据两个位置的经纬度，来计算两地的距离（单位为KM）
      * 参数为String类型
@@ -15,28 +17,25 @@ public final class DistanceUtil {
      * @param lng2 商家纬度
      * @return 两地的距离（单位为KM）
      */
-    public static long getDistance(String lat1Str, String lng1Str, String lat2Str, String lng2Str) {
-        Double lat1 = Double.parseDouble(lat1Str);
-        Double lng1 = Double.parseDouble(lng1Str);
-        Double lat2 = Double.parseDouble(lat2Str);
-        Double lng2 = Double.parseDouble(lng2Str);
-          
-        double radLat1 = rad(lat1);
-        double radLat2 = rad(lat2);
-        double difference = radLat1 - radLat2;
-        double mdifference = rad(lng1) - rad(lng2);
-        double distance = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(difference / 2), 2)
-                + Math.cos(radLat1) * Math.cos(radLat2)
-                * Math.pow(Math.sin(mdifference / 2), 2)));
-        distance = distance * EARTH_RADIUS;
-        distance = Math.round(distance * 10000) / 10000;
-        String distanceStr = distance+"";
-        distanceStr = distanceStr.
-            substring(0, distanceStr.indexOf("."));
-          
-        return Long.parseLong(distanceStr);
-    }
+	    public static double getDistance(String lat1Str, String lng1Str, String lat2Str, String lng2Str) {
+	        Double lat1 = Double.parseDouble(lat1Str);
+	        Double lng1 = Double.parseDouble(lng1Str);
+	        Double lat2 = Double.parseDouble(lat2Str);
+	        Double lng2 = Double.parseDouble(lng2Str);
+	        double radLat1 = rad(lat1);
+	        double radLat2 = rad(lat2);
+	        double difference = radLat1 - radLat2;
+	        double mdifference = rad(lng1) - rad(lng2);
+	        double distance = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(difference / 2), 2)
+	                + Math.cos(radLat1) * Math.cos(radLat2)
+	                * Math.pow(Math.sin(mdifference / 2), 2)));
+	        distance = distance * EARTH_RADIUS;
+	        distance=Math.round(distance*10)/10.0;
+	        String distanceStr = String.valueOf(distance);
+	        return Double.parseDouble(distanceStr);
+	    }
     
+	    
 	 /**
      * 根据两个位置的经纬度，来计算两地的距离（单位为KM）
      * 参数为double类型
@@ -46,7 +45,7 @@ public final class DistanceUtil {
      * @param lng2 商家纬度
      * @return 两地的距离（单位为KM）
      */
-    public static long getDistance(double lat1, double lng1, double lat2, double lng2) {     
+    public static double getDistance(double lat1, double lng1, double lat2, double lng2) {
         double radLat1 = rad(lat1);
         double radLat2 = rad(lat2);
         double difference = radLat1 - radLat2;
@@ -55,12 +54,9 @@ public final class DistanceUtil {
                 + Math.cos(radLat1) * Math.cos(radLat2)
                 * Math.pow(Math.sin(mdifference / 2), 2)));
         distance = distance * EARTH_RADIUS;
-        distance = Math.round(distance * 10000) / 10000;
-        String distanceStr = distance+"";
-        distanceStr = distanceStr.
-            substring(0, distanceStr.indexOf("."));
-          
-        return Long.parseLong(distanceStr);
+        distance=Math.round(distance*10)/10.0;
+        String distanceStr = String.valueOf(distance);
+        return Double.parseDouble(distanceStr);
     }
     
     private static double rad(double d) { 
